97755ce2cd2a1d1d8ad2f31652c14400306841df
rahulguna10 <rahulgu@synopsys.com>
2020-09-22 16:22:15 -0400

    removed project name from env

--- workflow_engine/loadVariables.sh
+++ workflow_engine/loadVariables.sh
@@ -3,9 +3,8 @@ export WORKFLOW_TEMPLATE_PATH="${1}/workflow_engine/workflow-template.yml"
 workflow=$(cat $WORKFLOW_TEMPLATE_PATH | \
 sed "s~{{SLACK_CHANNEL_ID}}~${2}~g; \
 s~{{SLACK_TOKEN}}~${3}~g; \
-s~{{POLARIS_PROJECT_NAME}}~${4}~g; \
-s~{{POLARIS_URL}}~${5}~g; \
-s~{{POLARIS_TOKEN}}~${6}~g")
+s~{{POLARIS_URL}}~${4}~g; \
+s~{{POLARIS_TOKEN}}~${5}~g")
 
 # apply the yml with the substituted value
 echo "$workflow" > ${1}/workflow_engine/workflow.yml
